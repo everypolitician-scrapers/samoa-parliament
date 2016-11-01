c03779e8fce62efdb0ce4f52b726105e777ef923
Oliver Denman <oliver@therewillbeanimals.com>
2016-11-01 16:28:56 +0000

    Capitalize tribal names of Prime Minister
    All members except the Prime Minister have their trible
    names listed in all-caps. This causes complications
    in scraping as the trible names are differentiated by
    their capitalization. To avoid complications we convert
    the trible names to uppercase before passing the document
    to Memberships.

--- scraper.rb
+++ scraper.rb
@@ -13,7 +13,11 @@ class String
 end
 
 URL = 'http://www.palemene.ws/new/members-of-parliament/members-of-the-xvi-parliament/'.freeze
-NOKO = Nokogiri::HTML(open(URL).read)
+# We need to tweak the document slightly as the trible names of one member are
+# not capitalized in the same way as the trible names of other members. We're doing this now
+# to avoid complications later
+BODY = open(URL).read.gsub('Fatialofa Lupesoliai Tuilaepa', 'FATIALOFA LUPESOLIAI TUILAEPA')
+NOKO = Nokogiri::HTML(BODY)
 
 Members.new(NOKO).to_h[:members].each do |member|
   ScraperWiki.save_sqlite([:name], member)
