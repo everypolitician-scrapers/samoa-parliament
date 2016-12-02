ead54909a29b61b3bf8c562e91294a544121a22d
Oliver Denman <oliver@therewillbeanimals.com>
2016-12-02 11:32:14 +0000

    Extract MemberSection class

---
+++ lib/member_section.rb
@@ -0,0 +1,40 @@
+require 'scraped'
+
+class MemberSection < Scraped::HTML
+  field :name do
+    name_and_party.split('(')[0].tidy
+  end
+
+  field :party_id do
+    party_id
+  end
+
+  field :party_name do
+    party_list[party_id]
+  end
+
+  field :area do
+    noko.xpath('td/text()')[2].text.tidy
+  end
+
+  field :term do
+    16
+  end
+
+  private
+
+  def party_id
+    name_and_party.split('(')[-1].gsub(')','')
+  end
+
+  def party_list
+    {
+      'H' => 'HRPP',
+      'TS' => 'Tautua Samoa'
+    }
+  end
+
+  def name_and_party
+    noko.xpath('td/text()')[1].text
+  end
+end
--- scraper.rb
+++ scraper.rb
@@ -7,6 +7,9 @@ require 'colorize'
 require 'pry'
 require 'open-uri/cached'
 OpenURI::Cache.cache_path = '.cache'
+require 'require_all'
+
+require_rel 'lib'
 
 class String
   def tidy
@@ -22,24 +25,11 @@ def scrape_list(url)
   warn url
   noko = noko_for(url)
   noko.xpath('//div[@class="entry"]/table[1]/tbody/tr').each do |a|
-    name_and_party = a.xpath('td/text()')[1].text
-    party_id = name_and_party.split('(')[-1].gsub(')','')
-    data = {
-      name: name_and_party.split('(')[0].tidy,
-      party_id: party_id,
-      party_name: party_list[party_id],
-      area: a.xpath('td/text()')[2].text.tidy,
-      term: 16
-    }
-    ScraperWiki.save_sqlite([:name], data)
+    ScraperWiki.save_sqlite(
+      [:name],
+      MemberSection.new(response: Scraped::Request.new(url: url).response, noko: a).to_h
+    )
   end
-end
-
-def party_list
-  {
-    'H' => 'HRPP',
-    'TS' => 'Tautua Samoa'
-  }
 end
 
 scrape_list('http://www.palemene.ws/new/members-of-parliament/members-of-the-xvi-parliament/')
