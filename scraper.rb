6dc304ee245c71514c53dd81f3aa8b9e3d51b6cf
Tony Bowden <tony@tmtm.com>
2017-05-09 07:36:30 +0100

    rubocop -a

--- scraper.rb
+++ scraper.rb
@@ -1,5 +1,6 @@
 #!/bin/env ruby
 # encoding: utf-8
+# frozen_string_literal: true
 
 require 'nokogiri'
 require 'pry'
@@ -10,7 +11,7 @@ OpenURI::Cache.cache_path = '.cache'
 
 class String
   def tidy
-    self.gsub(/[[:space:]]+/, ' ').strip
+    gsub(/[[:space:]]+/, ' ').strip
   end
 end
 
@@ -22,12 +23,12 @@ def scrape_list(url)
   warn url
   noko = noko_for(url)
   noko.css('div.ngg-gallery-thumbnail a').each do |a|
-    data = { 
-      id: a.attr('data-image-id'),
-      name: a.attr('data-title'),
+    data = {
+      id:    a.attr('data-image-id'),
+      name:  a.attr('data-title'),
       image: a.attr('data-src'),
     }
-    ScraperWiki.save_sqlite([:id, :name], data)
+    ScraperWiki.save_sqlite(%i[id name], data)
   end
 
   unless (next_page = noko.css('div.ngg-navigation a.next/@href')).empty?
