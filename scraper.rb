36b57c72e8b040813a6e4fb6ab864d495427482a
Tony Bowden <tony@tmtm.com>
2017-05-09 07:53:05 +0100

    Rewrite for new URL and layout

--- scraper.rb
+++ scraper.rb
@@ -9,27 +9,56 @@ require 'scraperwiki'
 require 'open-uri/cached'
 OpenURI::Cache.cache_path = '.cache'
 
-def noko_for(url)
-  Nokogiri::HTML(open(url).read)
-end
-
-def scrape_list(url)
-  warn url
-  noko = noko_for(url)
-  noko.css('div.ngg-gallery-thumbnail a').each do |a|
-    data = {
-      id:    a.attr('data-image-id'),
-      name:  a.attr('data-title'),
-      image: a.attr('data-src'),
-    }
-    puts data.reject { |_, v| v.to_s.empty? }.sort_by { |k, _| k }.to_h if ENV['MORPH_DEBUG']
-    ScraperWiki.save_sqlite(%i[id name], data)
-  end
-
-  unless (next_page = noko.css('div.ngg-navigation a.next/@href')).empty?
-    scrape_list(next_page.text) rescue binding.pry
+class MembersPage < Scraped::HTML
+  field :members do
+    noko.at_css('.entry table').css('tr').map do |tr|
+      fragment(tr => MemberRow).to_h
+    end
   end
 end
+
+class MemberRow < Scraped::HTML
+  field :id do
+    td[0].text.tidy
+  end
+
+  field :name do
+    name_field_parts['name']
+  end
+
+  field :area do
+    td[2].text.tidy
+  end
+
+  field :party_id do
+    name_field_parts['partyid']
+  end
+
+  field :party do
+    party_id
+  end
+
+  private
+
+  def td
+    noko.css('td')
+  end
+
+  # Tofa AUMUA Isaia Lameko (H)
+  #  bracketed part = party ID
+  def name_field_parts
+    td[1].text.tidy.match(/(?<name>.*) \((?<partyid>.*?)\)/)
+  end
+end
+
+def scraper(h)
+  url, klass = h.to_a.first
+  klass.new(response: Scraped::Request.new(url: url).response)
+end
+
+start = 'http://www.palemene.ws/new/members-of-parliament/members-of-the-xvi-parliament/'
+data = scraper(start => MembersPage).members
+data.each { |mem| puts mem.reject { |_, v| v.to_s.empty? }.sort_by { |k, _| k }.to_h } if ENV['MORPH_DEBUG']
 
 ScraperWiki.sqliteexecute('DROP TABLE data') rescue nil
-scrape_list('http://www.parliament.gov.ws/new/members-of-parliament/member/')
+ScraperWiki.save_sqlite(%i[id name], data)
